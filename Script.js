128ebdea90a9cb94d36b146ba892b643b4fe056f
Laura Andriamifidy <andrialolo08@gmail.com>
2026-09-22 08:36:23 +0200

    ajout d un menu de navigation

--- Script.js
+++ Script.js
@@ -1,15 +1,20 @@
+const yearSpan = document.getElementById('year');
+if (yearSpan) {
+  yearSpan.textContent = new Date().getFullYear();
+}
+
 const filter = document.getElementById('filter');
 const projects = document.querySelectorAll('.project');
 
-filter.addEventListener('change', () => {
-  const value = filter.value;
-  projects.forEach(project => {
-    if (value === 'all' || project.classList.contains(value)) {
-      project.style.display = 'block';
-    } else {
-      project.style.display = 'none';
-    }
+if (filter && projects.length > 0) {
+  filter.addEventListener('change', () => {
+    const value = filter.value;
+    projects.forEach(project => {
+      if (value === 'all' || project.classList.contains(value)) {
+        project.style.display = 'block';
+      } else {
+        project.style.display = 'none';
+      }
+    });
   });
-});
-
-
+}
